ac4ae060cc2364d9a1346b666cd35a7750d6b362
Fazt <fazt@faztweb.com>
2024-02-24 10:09:52 -0500

    csrf

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,3 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = ['localhost','web-production-17d9.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-17d9.up.railway.app']
